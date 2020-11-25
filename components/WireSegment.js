964827c30f20784cc393991ed63b9cfff86408c9
Blaise Gratton <blaisegratton@gmail.com>
2020-11-25 00:03:15 -0600

    displaying wire segment caps as circular svgs

--- components/WireSegment.js
+++ components/WireSegment.js
@@ -1,3 +1,5 @@
+// $('wire-segment').setAttribute('is-powered', 'true')
+
 const style = document.createElement('style')
 
 style.textContent = `
@@ -9,6 +11,12 @@ window.customElements.define('wire-segment', class extends HTMLElement {
   constructor () {
     super()
 
+    this.attributeChangeHandlers = {
+      'is-powered': (self, ...rest) => {
+        self.shadowRoot.querySelector('line').style.stroke = 'red'
+      }
+    }
+
     const shadowDOM = this.attachShadow({ mode: 'open' })
     shadowDOM.appendChild(style)
 
@@ -19,35 +27,51 @@ window.customElements.define('wire-segment', class extends HTMLElement {
       'y2': y2Attribute
     } = this.attributes
 
-    const x1 = x1Attribute.value
-    const y1 = y1Attribute.value
-    const x2 = x2Attribute.value
-    const y2 = y2Attribute.value
+    const x1 = parseInt(x1Attribute.value)
+    const y1 = parseInt(y1Attribute.value)
+    const x2 = parseInt(x2Attribute.value)
+    const y2 = parseInt(y2Attribute.value)
 
     const width = Math.abs(x2 - x1)
     const height = Math.abs(y2 - y1)
 
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
     svg.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xlink', 'http://www.w3.org/1999/xlink')
-    svg.setAttribute('width', width)
-    svg.setAttribute('height', height)
-    svg.setAttribute('viewbox', `0 0 ${width} ${height}`)
+    const circleCapRadius = 4  // circular caps at ends of segments
+    svg.setAttribute('width', width + circleCapRadius * 2)
+    svg.setAttribute('height', height + circleCapRadius * 2)
+    svg.setAttribute('viewbox', `0 0 ${width + circleCapRadius * 2}, ${height + circleCapRadius * 2}`)
 
     const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-    line.setAttribute('id', 'segment-1')
-    line.setAttribute('x1', x1)
-    line.setAttribute('x2', x2)
-    line.setAttribute('y1', y1)
-    line.setAttribute('y2', y2)
+    // line.setAttribute('id', 'segment-1')
+    line.setAttribute('x1', x1 + circleCapRadius)
+    line.setAttribute('x2', x2 + circleCapRadius)
+    line.setAttribute('y1', y1 + circleCapRadius)
+    line.setAttribute('y2', y2 + circleCapRadius)
     line.setAttribute('stroke', 'black')
     line.setAttribute('stroke-width', 2)
 
+    const segmentEnd1 = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+    segmentEnd1.setAttribute('cx', x1 + circleCapRadius)
+    segmentEnd1.setAttribute('cy', y1 + circleCapRadius)
+    segmentEnd1.setAttribute('r', circleCapRadius)
+    segmentEnd1.setAttribute('fill', 'black')
+
+    const segmentEnd2 = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+    segmentEnd2.setAttribute('cx', x2 + circleCapRadius)
+    segmentEnd2.setAttribute('cy', y2 + circleCapRadius)
+    segmentEnd2.setAttribute('r', circleCapRadius)
+    segmentEnd2.setAttribute('fill', 'black')
+
     svg.appendChild(line)
+    svg.appendChild(segmentEnd1)
+    svg.appendChild(segmentEnd2)
     shadowDOM.appendChild(svg)
   }
 
-  attributeChangedCallback (attribute, thing, value, anotherThing) {
-    this.shadowRoot.querySelector('line').style.stroke = 'red'
+
+  attributeChangedCallback (attribute, ...rest) {
+    this.attributeChangeHandlers[attribute](this, ...rest)
   }
 
   static get observedAttributes () {
