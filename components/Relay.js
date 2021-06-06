c762e6167d37f0134df2b1cf116199fb920805cd
Blaise Gratton <blaisegratton@gmail.com>
2021-06-06 15:11:14 -0500

    rename .test.js files; scaffold Relay files

---
+++ components/Relay.js
@@ -0,0 +1,10 @@
+if (typeof process !== 'undefined') {
+  const module = require('../components/ComponentContainer')
+  ComponentContainer = module.ComponentContainer
+} else if (typeof ComponentContainer === 'undefined') {
+  throw new Error('[Relay.js] ComponentContainer is undefined - did you load it before loading this script?')
+}
+
+window.customElements.define('simple-relay', class SimpleRelay extends ComponentContainer {
+
+}
