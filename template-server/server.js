c300bc34f4be3c6e250b2f3233451f126fbbcf03
Blaise Gratton <blaisegratton@gmail.com>
2021-11-30 17:52:59 -0600

    implement GET all and by template name; begin POST

--- template-server/server.js
+++ template-server/server.js
@@ -1,36 +1,88 @@
-import { readdirSync } from 'fs'
+import { readdirSync, readFileSync } from 'fs'
 import { createServer } from 'http'
 
 const htmlContentType = { 'Content-Type': 'text/html' }
 
+const handleGET = (req, res) => {
+  const [path, rawParams] = req.url.split('?')
+  const params = (
+    rawParams ?
+      rawParams.split('&').reduce((result, current) => {
+        const [key, val] = current.split('=')
+        result[key] = val
+        return result
+      }, {})
+      : {})
+
+  try {
+    if (path == '/') {
+      res.writeHead(200, htmlContentType)
+      const templates = readdirSync('./templates').filter(fileName => {
+        const parts = fileName.split('.')
+        const extension = parts[parts.length - 1]
+        return extension === 'html'
+      })
+      res.end(JSON.stringify(templates.map(f => `/templates/${f}`)))
+    } else if (path == '/templates') {
+      const fileName = params.file
+
+      if (!fileName) {
+        res.writeHead(400)
+        res.end('file name needed')
+      }
+
+      try {
+        const content = readFileSync(`./templates/${fileName}`)
+        res.writeHead(200, htmlContentType)
+        res.end(content)
+      } catch {
+        res.writeHead(404) && res.end()
+      }
+    } else res.writeHead(404) && res.end()
+  } catch (e) {
+    handleFailure(res, e)
+  }
+}
+
+const handlePOST = (req, res) => {
+  if (req.url == '/templates') {
+    let body = ''
+
+    req.on('data', data => {
+      body += data
+    })
+
+    req.on('end', () => {
+      try {
+        const { name, content } = JSON.parse(body)
+        console.log({ name, content })
+        res.writeHead(201, htmlContentType)
+        res.end()
+      } catch (e) {
+        handleFailure(res, e)
+      }
+    })
+  } else res.writeHead(404) && res.end()
+}
+
+const handleFailure = (res, error) => {
+  console.error(error)
+  res.writeHead(500, htmlContentType)
+  res.end(error.toString())
+}
+
+const handlers = {
+  'GET': handleGET,
+  'POST': handlePOST
+}
+
 const handleRequest = (req, res) => {
-  if (req.url !== '/') {
-    res.writeHead(404, htmlContentType)
-    res.end('not found')
-  } else if (req.method === 'GET')
-    handleGet(req, res)
-  else if (req.method === 'POST')
-    handlePost(req, res)
-}
-
-const handleGet = (req, res) => {
-  res.writeHead(200, htmlContentType)
-  res.end(JSON.stringify(readdirSync('./templates')))
-}
-
-const handlePost = (req, res) => {
-  let body = ''
-
-  req.on('data', data => {
-    body += data
-  })
-
-  req.on('end', () => {
-    const parsedBody = JSON.parse(body)
-    console.log(parsedBody)
-    res.writeHead(201, htmlContentType)
-    res.end()
-  })
+  try {
+    handlers[req.method](req, res)
+  } catch (e) {
+    res.writeHead(405, htmlContentType)
+    res.end(e.toString())
+  }
 }
 
 const server = createServer(handleRequest)
